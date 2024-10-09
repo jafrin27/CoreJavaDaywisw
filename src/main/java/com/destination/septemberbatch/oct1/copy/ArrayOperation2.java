52c9c19c9e0025d067757c35530192b9b2aec40c
Dell <Dell@DESKTOP-SDR5EU3>
2024-10-09 12:37:30 +0530

    All Previous Arrays

---
+++ src/main/java/com/destination/septemberbatch/oct1/copy/ArrayOperation2.java
@@ -0,0 +1,67 @@
+package com.destination.septemberbatch.oct1.copy;
+import java.util.Scanner;
+class Array2
+{
+	int arr[][];
+	int cls;
+	int stu;
+	void creatArray(int n,int m)
+	{
+		cls = n;
+		stu = m;
+		arr=new int[n][m];
+		System.out.println("Array is created successfully:");
+		System.out.println("=================================");
+		}
+	void adddata() 
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("collecting array data:");
+		for(int i=0;i<cls;i++) 
+		{
+			System.out.println("Inside class no: : "+(i+1));
+			for(int j=0;j<stu;j++)
+			{
+				System.out.println("Enter the marks of student no: "+(j+1));
+				arr[i][j]=sc.nextInt();
+			}
+		}
+		System.out.println("Marks Scuccessfully collected ");
+		System.out.println("==============================");
+		
+	}
+	void display()
+	{
+		System.out.println("Display array data: ");
+		for(int i=0;i<cls;i++)
+		{
+			System.out.println("Inside class no:"+(i+1));
+			for(int j=0;j<stu;j++)
+			{
+				System.out.println("The marks of student no: "+(j+1)+" is ="
+			+arr[i][j]);
+			}
+		}
+	}
+	
+}
+
+public class ArrayOperation2 
+{
+
+	public static void main(String[] args) 
+	{
+		Array2 a2= new Array2();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the class count: ");
+		int cls = sc.nextInt();
+		System.out.println("Enter the student count: ");
+		int stu = sc.nextInt();
+		a2.creatArray(cls, stu);
+		a2.adddata();
+		a2.display();
+
+
+	}
+
+}
---
+++ src/main/java/com/destination/septemberbatch/oct1/copy/ArrayOperation3.java
@@ -0,0 +1,68 @@
+package com.destination.septemberbatch.oct1.copy;
+import java.util.Scanner;
+class Array3
+{
+	int arr[][];
+	int cls;
+	Scanner sc = new Scanner(System.in);
+	
+	void createArray(int n)
+	{
+		cls = n;
+		arr = new int[n][];
+		System.out.println("Collecting student count for each class: ");
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println("Enter the student count inside class no:"+(i+1));
+			int stu = sc.nextInt();
+			arr[i] = new int[stu];
+		}
+		System.out.println("Array is created successfully");
+		System.out.println("=========================");
+	}
+	void addData()
+	{
+		System.out.println("Collecting array Data: ");
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println("Inside Class no: "+(i+1));
+			for(int j=0;j<arr[i].length;j++)
+			{
+				System.out.println("Enter the marks of student no: "+(j+1));
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println("Marks Scuccessfully collected: ");
+		System.out.println("==============");
+	}
+	void display()
+	{
+		System.out.println("Display array Data: ");
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println("Inside class no:"+(i+1));
+			for(int j=0;j<arr[i].length;j++) 
+			{
+				System.out.println("The marks of student no: "+(j+1)+" is"+arr[i][j]);
+			}
+		}
+	}
+}
+
+public class ArrayOperation3 
+{
+	
+
+	public static void main(String[] args) 
+	{
+		Array3 a3 = new Array3();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the class count : ");
+		int cls = sc.nextInt();
+		a3.createArray(cls);
+		a3.addData();
+		a3.display();
+
+	}
+
+}
